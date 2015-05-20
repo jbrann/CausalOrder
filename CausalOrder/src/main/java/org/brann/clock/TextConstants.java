28a9eb89faf51d8226b2de0f5d4c558809536024
John Brann <john_brann@freddiemac.com>
2015-05-20 17:05:58 +0100

    first commit to local git

---
+++ CausalOrder/src/main/java/org/brann/clock/TextConstants.java
@@ -0,0 +1,10 @@
+package org.brann.clock;
+
+public final class TextConstants {
+
+	public static final String LAMPORT_LOGICAL_CLOCK = "LLC";
+	public static final String VECTOR_CLOCK = "VC";
+	public static final String OWNER_PROCESS_ID = "O_PID";
+	public static final String PROCESS_ID = "PID";
+	public static final String FOREIGN_CLOCKS = "FC";
+}
---
+++ CausalOrder/src/test/java/org/brann/clock/test/TestLogicalClock.java
@@ -0,0 +1,111 @@
+/**
+ * 
+ */
+package org.brann.clock.test;
+
+import static org.junit.Assert.*;
+
+import org.brann.clock.LogicalClock;
+import org.brann.clock.TextConstants;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author f400670
+ *
+ */
+public class TestLogicalClock {
+
+	/**
+	 * Test method for {@link org.brann.clock.ClockOperations#tick()}.
+	 */
+	@Test
+	public void testTickIsLessThan() {
+		
+		LogicalClock a = new LogicalClock();
+		LogicalClock b = new LogicalClock();
+		
+		assertFalse(a.isLessThan(a));
+		
+		assertFalse(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		a.tick();
+		assertTrue(b.isLessThan(a));
+		assertFalse(a.isLessThan(b));
+		
+		a.tick();
+		assertTrue(b.isLessThan(a));
+		assertFalse(a.isLessThan(b));
+		
+		b.tick();
+		assertTrue(b.isLessThan(a));
+		assertFalse(a.isLessThan(b));
+		
+		b.tick();
+		assertFalse(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		b.tick();
+		assertTrue(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		assertFalse(a.isLessThan(a));
+	}
+
+	
+	/**
+	 * Test method for {@link org.brann.clock.LogicalClock#toJson(com.fasterxml.jackson.core.JsonGenerator)}.
+	 */
+	@Test
+	public void testToFromJson() {
+		
+		LogicalClock a = new LogicalClock();
+		for (int i=0;i<100;++i) a.tick();
+		
+		String aAsJson = a.toString();
+		LogicalClock b = new LogicalClock(aAsJson);
+		
+		assertFalse(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		a = new LogicalClock("{\""+TextConstants.LAMPORT_LOGICAL_CLOCK+"\":["+Integer.MAX_VALUE+","+Integer.MAX_VALUE+"]}");
+		b = new LogicalClock("{\""+TextConstants.LAMPORT_LOGICAL_CLOCK+"\":["+Integer.MAX_VALUE+","+Integer.MAX_VALUE+"]}");
+		
+		assertFalse(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		b.tick();
+		assertTrue(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		a.tick();
+		a.tick();
+		assertFalse(a.isLessThan(b));
+		assertTrue(b.isLessThan(a));
+		
+		a = new LogicalClock("not good json");
+		b = new LogicalClock();
+		
+		assertFalse(a.isLessThan(b));
+		assertFalse(b.isLessThan(a));
+		
+		
+	}
+	
+	/**
+	 * Test method for {@link org.brann.clock.LogicalClock#clone()}.
+	 */
+	@Test
+	public void testLogicalClockLogicalClock() {
+
+		LogicalClock a = new LogicalClock("{\""+TextConstants.LAMPORT_LOGICAL_CLOCK+"\":["+Integer.MAX_VALUE+","+Integer.MAX_VALUE+"]}");
+		LogicalClock clone = new LogicalClock(a);
+		
+		assertFalse(a.isLessThan(clone));
+		assertFalse(clone.isLessThan(a));
+		
+	}
+
+}
